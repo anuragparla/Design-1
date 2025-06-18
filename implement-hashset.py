445838f5c87e5c06a3d276aa276cf0b609068f94
anuragparla <anuragparla@gmail.com>
2025-06-18 01:42:28 -0400

    feat: design hashset without using builtin set function

---
+++ implement-hashset.py
@@ -0,0 +1,63 @@
+'''
+1) add()
+Time complexity: O(1)
+Space complexity: O(N)
+
+2) remove()
+Time complexity: O(1)
+Space complexity: O(1)
+
+3) contains()
+Time complexity: O(1)
+Space complexity: O(1)
+'''
+
+class MyHashSet:
+
+    def __init__(self):
+        self.hashSet = [None]*1000
+    
+    def getBucketIndex(self, key) -> int:
+        return key % 1000
+    
+    def getBucketItemIndex(self, key ) -> int:
+        return key // 1000
+
+    def add(self, key: int) -> None:
+        bucketIndex = self.getBucketIndex(key)
+        bucketItemIndex = self.getBucketItemIndex(key)
+
+        if self.hashSet[bucketIndex] is None:
+            if bucketIndex == 0:
+                self.hashSet[bucketIndex] = [False] * 1001
+            else:
+                self.hashSet[bucketIndex] = [False] * 1000
+            self.hashSet[bucketIndex][bucketItemIndex] = True
+        else:
+            self.hashSet[bucketIndex][bucketItemIndex] = True
+            
+        
+    def remove(self, key: int) -> None:
+        bucketIndex = self.getBucketIndex(key)
+        bucketItemIndex = self.getBucketItemIndex(key)
+        if self.hashSet[bucketIndex] is not None:
+            self.hashSet[bucketIndex][bucketItemIndex] = False
+    
+                    
+    def contains(self, key: int) -> bool:
+        bucketIndex = self.getBucketIndex(key)
+        bucketItemIndex = self.getBucketItemIndex(key)
+        if self.hashSet[bucketIndex] is None:
+            return False
+        if self.hashSet[bucketIndex][bucketItemIndex] == True:
+            return True
+        return False
+        
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
