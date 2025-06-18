bd1f84b299f02a14d250428bf3291067628d881c
anuragparla <anuragparla@gmail.com>
2025-06-18 07:32:17 -0400

    add meaningful comments to hashset logic

--- implement-hashset.py
+++ implement-hashset.py
@@ -16,13 +16,19 @@ class MyHashSet:
 
     def __init__(self):
         self.hashSet = [None]*1000
-    
+    '''
+    First hash function that returns the index of the bucket
+    '''
     def getBucketIndex(self, key) -> int:
         return key % 1000
-    
+    '''
+    Second hash function that returns the index of the bucket item
+    '''
     def getBucketItemIndex(self, key ) -> int:
         return key // 1000
-
+    '''
+    Add a key into the hashset
+    '''
     def add(self, key: int) -> None:
         bucketIndex = self.getBucketIndex(key)
         bucketItemIndex = self.getBucketItemIndex(key)
@@ -36,14 +42,18 @@ class MyHashSet:
         else:
             self.hashSet[bucketIndex][bucketItemIndex] = True
             
-        
+    '''
+    Remove a key from the hashset
+    '''
     def remove(self, key: int) -> None:
         bucketIndex = self.getBucketIndex(key)
         bucketItemIndex = self.getBucketItemIndex(key)
         if self.hashSet[bucketIndex] is not None:
             self.hashSet[bucketIndex][bucketItemIndex] = False
     
-                    
+    '''
+    Check if the key exists in the hashset
+    '''              
     def contains(self, key: int) -> bool:
         bucketIndex = self.getBucketIndex(key)
         bucketItemIndex = self.getBucketItemIndex(key)
